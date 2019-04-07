532556e74424d00b7063ece6f3f8da24943f5d9d
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-08 00:47:48 +0100

    Createa schema for category

---
+++ model/category.js
@@ -0,0 +1,28 @@
+//Import joi for validation
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+//Create a schema/model for the food category
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength:50
+  }
+});
+
+const Category = mongoose.model('Category',categorySchema);
+
+//validates the food category created 
+const validateCategory = (category) => {
+  const schema = {
+    name: Joi.string().min(5).max(50).required()
+  };
+
+  return Joi.validate(category,schema);
+};
+
+exports.categorySchema = categorySchema;
+exports.validate = validateCategory;
+exports.Category = Category;
